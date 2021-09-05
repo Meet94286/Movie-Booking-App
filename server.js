b006dd5e9163d8a04ace3c2ce95f2c033afc670e
Meet94286 <mehtameet4321.mm@gmail.com>
2021-09-05 20:16:33 +0530

    Added dynamic routes

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-let string = "server file";
+const http = require("http");
 
+port = 3000;
+
+const routeMap = {
+    "/movies" : "All Movies Data in JSON format from Mongo DB",
+    "/genres" : "All Genres Data in JSON format from Mongo DB",
+    "/artists" : "All Artists Data in JSON format from Mongo DB"
+}
+
+const app = http.createServer((req,res)=>{
+    if(routeMap[req.url]){
+        res.writeHead(200,{"Content-type": "text/html"});
+        res.end(routeMap[req.url]);
+    }else{
+        res.writeHead(404,{"Content-type": "plain/text"});
+        res.end()
+    }
+})
+
+app.listen(port);
+module.exports = {app,routeMap};
