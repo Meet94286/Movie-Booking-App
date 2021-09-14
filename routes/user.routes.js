1c6a894c349e13c6dd856c2fccdb00da01999b2a
Meet94286 <mehtameet4321.mm@gmail.com>
2021-09-14 19:58:12 +0530

    Added bookShow and getCouponCode routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,10 @@
-const {signUp,login, logout}= require("../controllers/user.controller");
+const {signUp,login, logout, getCouponCode, bookShow}= require("../controllers/user.controller");
 
 const userRouter = require("express").Router();
 
 userRouter.post("/signup",signUp);
 userRouter.post("/login",login)
 userRouter.post("/logout",logout)
+userRouter.post("/bookShow",bookShow);
+userRouter.get("/getCouponCode",getCouponCode);
 module.exports = userRouter;
